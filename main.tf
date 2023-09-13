fdc9c9d7947fadc276b6404d29ddc97b7af27179
truebala <131427463+truebala@users.noreply.github.com>
2023-09-13 18:35:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 provider "aws" {
-region = "us-east-1"
+region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_instance" "example" {
+ami = "ami-0b2b4f610e654d9ac"
+instance_type = "t2.micro"
+
 tags = {
-Name = "ajghai"
+Name = "bala"
 }
 }
